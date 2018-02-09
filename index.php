001c723909b6c4c4ec3778c1c6a2d14d048c1ffa
Joe Reid <joe@joereid.com>
2018-02-08 22:19:36 -0500

    Updating the contact icons

--- index.php
+++ index.php
@@ -12,11 +12,12 @@
 			</ul>	
 			
 			<ul id="contacts">
+				<li><a href="https://reid.ws" class="contact contact-li" id="contact-blog"><img src="/assets/img/contact-bl.svg" title="my blog" alt="my blog"></a></li>
 				<li><a href="https://www.facebook.com/jar3817" class="contact contact-fb"><img src="/assets/img/contact-fb.svg" title="facebook" alt="facebook"></a></li>
-				<li><a href="mailto:joe@joereid.com" class="contact contact-em"><img src="/assets/img/contact-em.svg" title="email" alt="email"></a></li>
+				<li><a href="mailto:joe@joereid.com" class="contact contact-em"><img src="/assets/img/contact-em.svg" title="email me" alt="email me"></a></li>
 				<li><a href="https://www.linkedin.com/in/jreid-roc" class="contact contact-li"><img src="/assets/img/contact-li.svg" title="linkedin" alt="linkedin"></a></li>
-				<li><a href="https://www.github.com/jar3817" class="contact contact-li"><img src="/assets/img/contact-gh.svg" title="Github" alt="Github"></a></li>
-				<li><a href="https://reid.ws" class="contact contact-li" id="contact-blog"><img src="/assets/img/contact-bl.svg" title="blog" alt="blog"></a></li>
+				<li><a href="https://www.github.com/jar3817" class="contact contact-li"><img src="/assets/img/contact-gh.svg" title="github" alt="github"></a></li>
+				
 			</ul>
 		</div>
 <?php
